7bcd302a491ee7974053e7f365d4f470778bb221
Rashid Menhas <rashidmenhas10@gmail.com>
2022-12-31 12:09:11 +0500

    Create expenses controller and also make action.

---
+++ app/controllers/expenses_controller.rb
@@ -0,0 +1,36 @@
+class ExpensesController < ApplicationController
+  before_action :set_user
+
+  def index
+    @groups = @author.groups
+    @group = @groups.find(params[:group_id])
+    @expenses = @group.expenses
+  end
+
+  def new
+    @groups = @author.groups
+    @group = @groups..find(params[:group_id])
+    @expense = Expense.new
+  end
+  
+  def create
+    @group = Group.find(params[:group_id])
+    @expense = @author.expenses.new(expense_params)
+    return unless @expense.save
+    @group_expense = GroupExpense.new(group_id: @group.id, expense_id: @expense.id)
+    return unless @group_expenses.save
+    
+    redirect_to group_expense_path(@group)
+  end
+  
+  private
+
+  def expense_params
+    params.require(:expense).permit(:name, :amount)
+  end
+
+  def set_user
+    @author = current_user
+  end
+  
+end
