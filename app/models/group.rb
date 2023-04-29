55acf94161199be9b6818778b7139f22f4963b75
Rashid Menhas <rashidmenhas10@gmail.com>
2023-04-29 23:34:49 +0500

    Create rspec test.

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,5 +4,4 @@ class Group < ApplicationRecord
   has_many :group_expenses
   has_many :expenses, through: :group_expenses
   validates :name, presence: true, length: { maximum: 50 }
-  validates :icon, presence: true
 end
